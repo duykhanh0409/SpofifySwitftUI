d7d0096a96c4460b749b7a7fe9ed5d58652d4fd7
KhanhNguyen49 <duykhanhlb4999@gmail.com>
2024-10-05 16:42:07 +0700

    first commit

--- SpotifySwiftUI/ContentView.swift
+++ SpotifySwiftUI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Nguyen Ngoc Duy Khanh")
         }
         .padding()
     }
