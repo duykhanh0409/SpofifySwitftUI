ad42cbc36a432640f3c0e7102a9bb31c416247ad
KhanhNguyen <duykhanh.nguyen@unifiedpost.com>
2024-10-05 16:27:53 +0700

    Initial Commit

---
+++ SpotifySwiftUI/SpotifySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifySwiftUIApp.swift
+//  SpotifySwiftUI
+//
+//  Created by Khanh Nguyen on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
