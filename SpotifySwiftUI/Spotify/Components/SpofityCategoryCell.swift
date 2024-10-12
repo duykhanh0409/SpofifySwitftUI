8d427bad3470a5cdd48bc55d3fdc9137ab31dbb9
KhanhNguyen49 <duykhanhlb4999@gmail.com>
2024-10-12 21:39:26 +0700

    feat: create SpotifyHomeView

---
+++ SpotifySwiftUI/Spotify/Components/SpofityCategoryCell.swift
@@ -0,0 +1,21 @@
+//
+//  SpofityCategoryCell.swift
+//  SpotifySwiftUI
+//
+//  Created by Khanh Nguyen on 12/10/24.
+//
+
+import SwiftUI
+
+struct SpofityCategoryCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        SpofityCategoryCell()
+    }
+}
--- SpotifySwiftUI/Spotify/Core/SpotifyHomeView.swift
+++ SpotifySwiftUI/Spotify/Core/SpotifyHomeView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct SpotifyHomeView: View {
+    
+    @State private var currentUser:User? = nil
+    
     var body: some View {
         ZStack {
             Color.spotifyBlack.ignoresSafeArea()
+            HStack(content: {
+                if let currentUser {
+                    ImageLoaderView(urlString: currentUser.image)
+                        .frame(width: 30, height: 30)
+                        .background(.spotifyWhite)
+                        .clipShape(.circle)
+                }
+                ScrollView(.horizontal) {
+                    HStack(spacing: 8, content: {
+                        ForEach(0..<20) { _ in
+                            Rectangle()
+                                .fill(Color.red)
+                                .frame(width: 10, height: 10)
+                        }
+                    })
+                }
+                .scrollIndicators(.hidden)
+            })
+        }
+        .task {
+            await getData()
+        }
+    }
+    
+    private func getData() async{
+        do{
+            currentUser = try await DatabaseHelper().getUser().first
+          
+        }catch {
+            
         }
     }
 }
