f6bdeb5f8869dfa6a1811eaeed999332f79aa74c
KhanhNguyen49 <duykhanhlb4999@gmail.com>
2024-10-12 21:18:26 +0700

    feat: create folder core and components

---
+++ SpotifySwiftUI/Spotify/Core/SpotifyHomeView.swift
@@ -0,0 +1,20 @@
+//
+//  SpotifyHomeView.swift
+//  SpotifySwiftUI
+//
+//  Created by Khanh Nguyen on 12/10/24.
+//
+
+import SwiftUI
+
+struct SpotifyHomeView: View {
+    var body: some View {
+        ZStack {
+            Color.spotifyBlack.ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    SpotifyHomeView()
+}
