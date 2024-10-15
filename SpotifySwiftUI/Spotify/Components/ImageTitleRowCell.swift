dcfed3dfbe67fc832984bd0fb90c3f6e2434ee0a
KhanhNguyen49 <duykhanhlb4999@gmail.com>
2024-10-15 21:15:49 +0700

    feat: initial ImageTitleRowCell component

---
+++ SpotifySwiftUI/Spotify/Components/ImageTitleRowCell.swift
@@ -0,0 +1,31 @@
+//
+//  ImageTitleRowCell.swift
+//  SpotifySwiftUI
+//
+//  Created by Khanh Nguyen on 15/10/24.
+//
+
+import SwiftUI
+
+struct ImageTitleRowCell: View {
+    var imageSize: CGFloat = 100
+    var imageName: String = Constants.romdomImage
+    var title:String = "Some Item Name"
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            ImageLoaderView(urlString: imageName)
+                .frame(width: imageSize, height: imageSize)
+            Text(title)
+                .font(.callout)
+                .foregroundStyle(.spotifyLightGray)
+        }
+    }
+}
+
+#Preview {
+    ZStack{
+        Color.black.ignoresSafeArea()
+        ImageTitleRowCell()
+    }
+}
